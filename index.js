fcd44f72bd4915dd9f964929a51aa720c6bdb954
bankmarkaziordak <sg.bendy.sg@gmail.com>
2024-08-29 13:07:57 +0330

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Bank Markazi Ordak","Ordak Central Bank"];
 
 
 let currentIndex = 0;
